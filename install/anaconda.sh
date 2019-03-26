86e06698ed9e93af846a2bb58fd5245af4559794
Ferry <eplious.fan@gmail.com>
2019-03-26 19:03:58 +0800

    Update anaconda.sh

--- install/anaconda.sh
+++ install/anaconda.sh
@@ -3,7 +3,7 @@
 set -e
 echo "Install anaconda2 4.4"
 mkdir -p /tmp/anaconda_dir
-cd /tmp/anacnda_dir
+cd /tmp/anaconda_dir
 wget https://repo.continuum.io/archive/Anaconda2-4.4.0-Linux-x86_64.sh -O install.sh
 /bin/bash install.sh -b -p /usr/local/anaconda2 -f
 cd /
